3a747a06c25aace2f15e93a8d48ccc55f24cef6d
Stefan Alfbo <stefan@alfbo.se>
2025-12-27 22:33:05 +0100

    Implement combat

--- src/systems/mod.rs
+++ src/systems/mod.rs
@@ -1,6 +1,6 @@
 use crate::prelude::*;
 
-mod collisions;
+mod combat;
 mod end_turn;
 mod entity_render;
 mod hud;
@@ -23,10 +23,10 @@ pub fn build_input_scheduler() -> Schedule {
 
 pub fn build_player_scheduler() -> Schedule {
     Schedule::builder()
+        .add_system(combat::combat_system())
+        .flush()
         .add_system(movement::movement_system())
         .flush()
-        .add_system(collisions::collisions_system())
-        .flush() // apply changes immediately
         .add_system(map_render::map_render_system())
         .add_system(entity_render::entity_render_system())
         .add_system(hud::hud_system())
@@ -37,7 +37,9 @@ pub fn build_player_scheduler() -> Schedule {
 pub fn build_monster_scheduler() -> Schedule {
     Schedule::builder()
         .add_system(random_move::random_move_system())
-        .flush() // apply changes immediately
+        .flush()
+        .add_system(combat::combat_system())
+        .flush()
         .add_system(movement::movement_system())
         .flush()
         .add_system(map_render::map_render_system())
