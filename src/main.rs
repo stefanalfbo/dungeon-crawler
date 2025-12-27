5daa1131a686d89c2fb83bafa728881436927ff8
Stefan Alfbo <stefan@alfbo.se>
2025-12-27 11:29:13 +0100

    Render an initial map for the game

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,28 @@ mod prelude {
 
 use prelude::*;
 
-struct State {}
+struct State {
+    map: Map,
+}
+
+impl State {
+    fn new() -> Self {
+        Self { map: Map::new() }
+    }
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print(1, 1, "Hello, Bracket Terminal!");
+        self.map.render(ctx);
     }
 }
 
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
-        .with_title("Dungeon Crawl")
+        .with_title("Dungeon Crawler")
+        .with_fps_cap(30.0)
         .build()?;
 
-    main_loop(context, State {})
+    main_loop(context, State::new())
 }
