56346d3cb288a09417f01464f0375930863569fc
Stefan Alfbo <stefan@alfbo.se>
2025-12-27 11:18:43 +0100

    Setup map.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
-use bracket_lib::prelude::*;
+mod map;
+
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+}
+
+use prelude::*;
 
 struct State {}
 
