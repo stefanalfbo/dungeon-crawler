2230e033046b64d78d3f90b997e4620b9955d8d3
Stefan Alfbo <stefan@alfbo.se>
2025-12-28 10:14:11 +0100

    Make it possible to heal as a player

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -1,8 +1,10 @@
 use crate::prelude::*;
 
 #[system]
-#[write_component(Point)]
+#[read_component(Point)]
 #[read_component(Player)]
+#[read_component(Enemy)]
+#[write_component(Health)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
@@ -10,14 +12,15 @@ pub fn player_input(
     #[resource] turn_state: &mut TurnState,
 ) {
     let mut players = <(Entity, &Point)>::query().filter(component::<Player>());
+    let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
 
-    if let Some(key) = key {
+    if let Some(key) = *key {
         let delta = match key {
             VirtualKeyCode::Left => Point::new(-1, 0),
             VirtualKeyCode::Right => Point::new(1, 0),
             VirtualKeyCode::Up => Point::new(0, -1),
             VirtualKeyCode::Down => Point::new(0, 1),
-            _ => Point::zero(),
+            _ => Point::new(0, 0),
         };
 
         let (player_entity, destination) = players
@@ -25,7 +28,7 @@ pub fn player_input(
             .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
             .unwrap();
 
-        let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+        let mut did_something = false;
         if delta.x != 0 || delta.y != 0 {
             let mut hit_something = false;
             enemies
@@ -33,6 +36,7 @@ pub fn player_input(
                 .filter(|(_, pos)| **pos == destination)
                 .for_each(|(entity, _)| {
                     hit_something = true;
+                    did_something = true;
 
                     commands.push((
                         (),
@@ -44,6 +48,7 @@ pub fn player_input(
                 });
 
             if !hit_something {
+                did_something = true;
                 commands.push((
                     (),
                     WantsToMove {
@@ -51,6 +56,15 @@ pub fn player_input(
                         destination,
                     },
                 ));
+            }
+        };
+        if !did_something {
+            if let Ok(health) = ecs
+                .entry_mut(player_entity)
+                .unwrap()
+                .get_component_mut::<Health>()
+            {
+                health.current = i32::min(health.max, health.current + 1);
             }
         }
         *turn_state = TurnState::PlayerTurn;
