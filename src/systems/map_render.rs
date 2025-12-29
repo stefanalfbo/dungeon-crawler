64b457c9e624771e3041496000aae7595f5a4ceb
Stefan Alfbo <stefan@alfbo.se>
2025-12-29 11:21:08 +0100

    Add spatial memory of the maze when player is exploring the dungeons

--- src/systems/map_render.rs
+++ src/systems/map_render.rs
@@ -3,7 +3,7 @@ use crate::prelude::*;
 #[system]
 #[read_component(FieldOfView)]
 #[read_component(Player)]
-pub fn map_render(ecs: &SubWorld, #[resource] map: &Map, #[resource] camera: &Camera) {
+pub fn map_render(#[resource] map: &Map, #[resource] camera: &Camera, ecs: &SubWorld) {
     let mut fov = <&FieldOfView>::query().filter(component::<Player>());
     let mut draw_batch = DrawBatch::new();
 
@@ -15,14 +15,21 @@ pub fn map_render(ecs: &SubWorld, #[resource] map: &Map, #[resource] camera: &Ca
         for x in camera.left_x..camera.right_x {
             let pt = Point::new(x, y);
             let offset = Point::new(camera.left_x, camera.top_y);
-            if map.in_bounds(pt) && player_fov.visible_tiles.contains(&pt) {
-                let idx = map_idx(x, y);
+            let idx = map_idx(x, y);
+            if map.in_bounds(pt)
+                && (player_fov.visible_tiles.contains(&pt) | map.revealed_tiles[idx])
+            {
+                let tint = if player_fov.visible_tiles.contains(&pt) {
+                    WHITE
+                } else {
+                    DARK_GRAY
+                };
                 match map.tiles[idx] {
                     TileType::Floor => {
-                        draw_batch.set(pt - offset, ColorPair::new(WHITE, BLACK), to_cp437('.'));
+                        draw_batch.set(pt - offset, ColorPair::new(tint, BLACK), to_cp437('.'));
                     }
                     TileType::Wall => {
-                        draw_batch.set(pt - offset, ColorPair::new(WHITE, BLACK), to_cp437('#'));
+                        draw_batch.set(pt - offset, ColorPair::new(tint, BLACK), to_cp437('#'));
                     }
                 }
             }
