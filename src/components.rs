251d74e75ced4d846fe0642dfa371b19c45cd230
Stefan Alfbo <stefan@alfbo.se>
2025-12-29 18:48:53 +0100

    Spawn other things than monster like magic and healing potion

--- src/components.rs
+++ src/components.rs
@@ -71,3 +71,11 @@ impl FieldOfView {
         }
     }
 }
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct ProvidesHealing {
+    pub amount: i32,
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct ProvidesDungeonMap;
